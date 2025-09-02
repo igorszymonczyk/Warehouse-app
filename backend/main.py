20eae5dabf3a4737bd5790cb605ec959b486ce6d
TomProc <tomek2003gl@gmail.com>
2025-09-02 21:19:14 +0200

    Backend/ login, register, authorization

---
+++ backend/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from routes.auth import router as auth_router
+from database import engine, Base
+# Utworzenie instancji FastAPI
+app = FastAPI(
+    title="Warehouse App API",
+    description="Backend do zarządzania magazynem",
+    version="1.0.0"
+)
+
+Base.metadata.create_all(bind=engine)
+# Dodanie routera auth
+app.include_router(auth_router)
+
+# Opcjonalny root endpoint
+@app.get("/")
+def read_root():
+    return {"message": "Warehouse App API działa!"}
---
+++ backend/models/users.py
@@ -0,0 +1,45 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, CheckConstraint
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, nullable=False, index=True)
+    password_hash = Column(String, nullable=False)
+    role = Column(String, nullable=False)
+
+class Admin(Base):
+    __tablename__ = "admins"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    imie = Column(String, nullable=False)
+    nazwisko = Column(String, nullable=False)
+
+
+class Seller(Base):
+    __tablename__ = "sellers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    imie = Column(String, nullable=False)
+    nazwisko = Column(String, nullable=False)
+
+class Warehouse_worker(Base):
+    __tablename__ = "warehouse_workers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    imie = Column(String, nullable=False)
+    nazwisko = Column(String, nullable=False)
+
+class Customer(Base):
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    imie = Column(String, nullable=False)
+    nazwisko = Column(String, nullable=False)
+
