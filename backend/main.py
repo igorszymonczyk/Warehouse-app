289f072ff2162ff900f0afae47a79f1e091e6598
TomProc <tomek2003gl@gmail.com>
2025-10-05 19:50:10 +0200

    New admin endpoints- UpdateUserRole, DeleteUser

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routes.auth import router as auth_router
 from database import engine, Base
+from routes.admin import router as admin_router
 # Utworzenie instancji FastAPI
 app = FastAPI(
     title="Warehouse App API",
@@ -11,6 +12,7 @@ app = FastAPI(
 Base.metadata.create_all(bind=engine)
 # Dodanie routera auth
 app.include_router(auth_router)
+app.include_router(admin_router)
 
 # Opcjonalny root endpoint
 @app.get("/")
---
+++ backend/routes/admin.py
@@ -0,0 +1,91 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from database import get_db
+from models.users import User
+from utils.tokenJWT import get_current_user
+from schemas.user import RoleUpdate
+from models import users as models
+from schemas import user as schemas
+
+router = APIRouter(
+    prefix="/admin",
+    tags=["Admin"]
+)
+
+
+#wyświtlanie zarejestrowanych kont
+@router.get("/users", response_model=list[schemas.UserResponse])
+def get_all_users(
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(get_current_user)
+):
+    # dostęp tylko dla admina
+    if current_user.role != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Admin access required"
+        )
+
+    users = db.query(models.User).all()
+    return users
+
+@router.put("/users/{user_id}/role")
+def update_user_role(
+    user_id: int,
+    new_role: RoleUpdate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    # dostęp tylko dla admina
+    if current_user.role != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Admin access required"
+        )
+
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+
+    user.role = new_role.role
+    db.commit()
+    db.refresh(user)
+
+    return {"message": f"User {user.email} role updated to {user.role}"}
+
+
+@router.delete("/users/{user_id}")
+def delete_user(
+    user_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user)
+):
+    # tylko admin może usuwać konta
+    if current_user.role != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Admin access required"
+        )
+
+    # znajdź użytkownika
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found"
+        )
+
+    #  zabezpieczenie — admin nie może usunąć samego siebie
+    if user.id == current_user.id:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="You cannot delete your own account"
+        )
+
+    db.delete(user)
+    db.commit()
+
+    return {"message": f"User {user.email} has been deleted"}
